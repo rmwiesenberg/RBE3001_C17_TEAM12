059303cc4fb65a3f821298431aff257ad37f053f
ItsRayK <xboxer221@live.com>
2017-01-21 16:03:24 -0500

    Fixed things. USARTDebug.c created

--- Lab1/Release/USART.d
+++ Lab1/Release/USART.d
@@ -1,9 +1,9 @@
-USART.o: ../USART.c ../../RBELib/RBELib.h ../../RBELib/ADC.h \
- ../../RBELib/DAC.h ../../RBELib/Debug.h ../../RBELib/motors.h \
- ../../RBELib/USARTDebug.h ../../RBELib/timer.h ../../RBELib/Periph.h \
- ../../RBELib/pot.h ../../RBELib/PID.h ../../RBELib/reg_structs.h \
- ../../RBELib/ports.h ../../RBELib/SPI.h ../../RBELib/SetServo.h \
- ../../RBELib/SlaveSelects.h
+USART.o USART.o: ../USART.c ../../RBELib/RBELib.h ../../RBELib/ADC.h \
+  ../../RBELib/DAC.h ../../RBELib/Debug.h ../../RBELib/motors.h \
+  ../../RBELib/USARTDebug.h ../../RBELib/timer.h ../../RBELib/Periph.h \
+  ../../RBELib/pot.h ../../RBELib/PID.h ../../RBELib/reg_structs.h \
+  ../../RBELib/ports.h ../../RBELib/SPI.h ../../RBELib/SetServo.h \
+  ../../RBELib/SlaveSelects.h
 
 ../../RBELib/RBELib.h:
 
