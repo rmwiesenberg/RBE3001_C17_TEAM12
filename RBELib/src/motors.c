926bb73b25865a354bacbcea57362c2c81142088
ItsRayK <xboxer221@live.com>
2017-01-31 21:17:54 -0500

    started motors.c

---
+++ RBELib/src/motors.c
@@ -0,0 +1,68 @@
+/*
+ * motors.c
+ *
+ *  Created on: Jan 31, 2017
+ *      Author: Rayyan
+ */
+
+
+void stopMotors(){
+
+}
+
+/**
+ * @brief Drive the arm to a desired angle
+ *
+ * @param lowerTheta The desired angle for the lower link.
+ * @param upperTheta The desired angle for the upper link.
+ *
+ * @todo Make a way to drive the links to a desired angle.
+ */
+void gotoAngles(int lowerTheta, int upperTheta){
+
+}
+
+/**
+ * @brief Drive the end effector of the arm to a desired X and Y position in the workspace.
+ *
+ * @param x The desired x position for the end effector.
+ * @param y The desired y position for the end effector.
+ *
+ * @todo Use kinematic equations to move the end effector to the desired position.
+ */
+void gotoXY(int x, int y){
+
+}
+
+/**
+ * @brief Drive a link (upper or lower) in a desired direction.
+ *
+ * @param link Which link to control.
+ * @param dir Which way to drive the link.
+ *
+ * @todo Create a way to drive either link in any direction.
+ */
+void driveLink(int link, int dir){
+//-4095 -> 4095
+	if(link == 1){
+		setDAC(0,0);
+		setDAC(1,20);
+	}
+
+	if(link == 2){
+
+	}
+
+
+}
+
+/**
+ * @brief Drive the arm to a "home" position using the potentiometers.  This
+ * should be called before using the encoders and just goes to a default position.
+ * Once this has been called once, you can initialize/clear the encoders.
+ *
+ * @todo Drive the arm to a known position using the potentiometers.
+ */
+void homePos(){
+
+}
