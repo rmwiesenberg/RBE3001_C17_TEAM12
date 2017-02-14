d6bc51c0e0a509940badb88d7340c772588e815f
Ryan Wiesenberg <rmwiesenberg@users.noreply.github.com>
2017-02-14 00:13:23 -0500

    Update ADC.c

--- RBELib/src/ADC.c
+++ RBELib/src/ADC.c
@@ -1,49 +1,80 @@
 /*
  * ADC.c
  *
- *  Created on: Jan 22, 2017
- *      Author: Rayyan Khan, Ben Titus, Ryan Wiesenberg
+ *  Created on: Jul 14, 2014
+ *      Author: ewillcox
  */
 
-#include <RBELib/RBELib.h>
-#include <avr/io.h>
+#include "RBELib/RBELib.h"
 
+/*
+ * 50khz - 200khz
+ * Free running - sample constantly
+ * 13 cycles a conversion - 25 1st
+ * P 246 - proper changing of channels
+ *
+ * ADC = (VIN * 1024) / Vref
+ */
+void initADC(int channel){
+  //Bits 7 - 6: Coupling capacitor at AREF
+  //Bit 5: No left adjustment
+  //Bits 4 - 0: Channel selection (0 - 7 for single ended)
+  ADMUX = (0x40) | channel;
 
-void initADC(int channel) {
-	cli();
+  //Bit 7: Enable ADC
+  //Bit 6: Starts conversions
+  //Bit 5: Auto trigger enable
+  //Bit 4: Interrupt flag telling conversions are complete
+  //Bit 3: Interrupt enable
+  //Bits 2 - 0: Prescaler of 128 (18432000 / 128 = 144kHz)
+  ADCSRA |= (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0); // Set ADC prescalar to 128 - 125KHz sample rate @ 16MHz
 
-	DDRA &= ~(1 << channel);
+  ADCSRA |= (1 << ADEN);  // Enable ADC
+  ADCSRA |= (1 << ADSC);  // Start A2D Conversions
+  //ADCSRA = 0xFF;
 
-	ADCSRA |= (1 << ADEN) | (0 << ADIE) | (7 << ADPS0);
+  //Bit 7: Reserved
+  //Bit 6: Analog Comparator Multiplexer Enable (ACME) - leave at 1
+  //Bit 5 - 3: Reserved
+  //Bit 2 - 0: Free running mode
+  ADCSRB = 0x40;
 
-	ADMUX = (1 << REFS0)|channel;
+  //Testing with port 7
+  DDRA = 0x00;
+}
 
-	DIDR0 |= 0 << channel;
+void clearADC(int channel)
+{
+	//untested, BAsically never needed
+	  ADMUX = (0x40) & channel;
+}
 
-	sei();
+unsigned short getADC(int channel)
+{
+// select the corresponding channel 0~7
+// ANDing with '7' will always keep the value
+// of 'channel' between 0 and 7
+channel &= 0b00000111;  // AND operation with 7
+ADMUX = (ADMUX & 0xF8)|channel;     // clears the bottom 3 bits before ORing
 
+// start single conversion
+// write '1' to ADSC
+ADCSRA |= (1<<ADSC);
+
+// wait for conversion to complete
+// till then, run loop continuously
+while(!(ADCSRA & (1<<ADIF)));
+//Clear ADIF by writing one to it
+ADCSRA|=(1<<ADIF);
+
+return (ADC);
 }
 
 
-void clearADC(int channel) {
-	ADCSRA &= ~(1 << ADEN);
-	ADC = 0;
+void changeADC(int channel){
+  //Change the channel using the same setting from initADC()
+  ADMUX = (0x40) | channel;
 }
 
 
-unsigned int getADC(int channel) {
-	ADCSRA &= ~(1 << ADATE);
-	ADMUX &= ~31;
-	ADMUX |= channel;	// set channel
-	ADCSRA |= (1 << ADSC); // start conversion
 
-	while(ADCSRA & (1 << ADSC)) continue; // poll for cleared start-conversion bit
-
-	return ADCW;
-}
-
-
-void changeADC(int channel) {
-	ADMUX &= ~31;
-	ADMUX |= channel;
-}
