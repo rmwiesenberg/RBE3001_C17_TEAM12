fdc58777a5bf3885bfcf22212b76e3f132401475
Ryan Wiesenberg <rmwiesenberg@users.noreply.github.com>
2017-02-14 00:51:51 -0500

    Update PID.c

--- RBELib/src/PID.c
+++ RBELib/src/PID.c
@@ -1,18 +1,12 @@
-/**
- * @file PID.h
+/*
+ * PID.c
  *
- * @brief The header file for PID constants and calculations.
- * @details Sets the PID constants and calculate the PID value.
- * @author Eric Willcox
- * @date August 17 2013
+ *  Created on: Aug 24, 2016
+ *      Author: joest
  */
 
-#include <RBELib/RBELib.h>
-#define NUMERRS 32
-#define SAMPLEFREQ 100
 
-pidConst pidConsts;
-
+#include "RBELib\RBELib.h"
 /**
  * @brief Sets the Kp, Ki, and Kd values for 1 link.
  * @details to set the values, use the following style
@@ -24,97 +18,69 @@ pidConst pidConsts;
  *
  * @todo Create a function to the the PID constants for a given link.
  */
-void setConst(char link, float Kp, float Ki, float Kd) {
-	if (link == 'H') {
+//long pidErrorSum = 0;
+long pidLastError_L = 0;
+long pidPrevLastError_L = 0;
+long pidLastError_H = 0;
+long pidPrevLastError_H = 0;
+long pidPrev_L = 0;
+long pidPrev_H = 0;
+float pidTd_H = 0;
+float pidTi_inv_H = 0;
+float pidTd_L = 0;
+float pidTi_inv_L = 0;
+pidConst pidConsts;
+void setConst(char link, float Kp, float Ki, float Kd)
+{
+	switch(link)
+	{
+	case 'H':
 		pidConsts.Kp_H = Kp;
 		pidConsts.Ki_H = Ki;
-		pidConsts.Kd_H = Kd;
-	}
-
-	if (link == 'L') {
+		pidConsts.Kd_H = Kp;
+		pidTd_H = Kd / Kp;
+		pidTi_inv_H = (Ki / Kp);
+		break;
+	case 'L':
 		pidConsts.Kp_L = Kp;
 		pidConsts.Ki_L = Ki;
 		pidConsts.Kd_L = Kd;
+		pidTd_L = Kd / Kp;
+		pidTi_inv_L = (Ki / Kp);
+		break;
 	}
-}
 
-signed int pastErr_H[NUMERRS];
-signed int pastErr_L[NUMERRS];
-signed int pastPID[NUMERRS];
+}
 
 /**
  * @brief Calculate the PID value.
- * @param  link Which link to calculate the error for (Use 'U' and 'L').
+ * @param  link Which link to calculate the error for (Use 'H' and 'L').
  * @param setPoint The desired position of the link.
  * @param actPos The current position of the link.
  *
  * @todo Make a function to calculate the PID value for a link.
  */
-signed int calcPID(char link, int setPoint, int actPos) {
+signed int calcPID(char link, int setPoint, int actPos)
+{
+	long error = actPos - setPoint;
+	long pidValue = 0;
+	if (link == 'L')
+	{
+		pidValue = pidPrev_L + pidConsts.Kp_L * ((1 + pidTi_inv_L + pidTd_L) * error  + (-1 - 2 * pidTd_L) * pidLastError_L + pidTd_L * pidPrevLastError_L);
+		pidPrev_L = pidValue;
+		pidPrevLastError_L = pidLastError_L;
+		pidLastError_L = error;
 
-	// calc current error
-	int error = setPoint - actPos;
-	int errSum = 0;
-	int i = 0;
-	long pVal = 0;
-	long iVal = 0;
-	long dVal = 0;
 
-	//pVal + iVal + dVal for high link
-	if (link == 'H') {
-
-		// put current error into array and shift down
-		for (i = NUMERRS; i > 0; i--) {
-			errSum += pastErr_H[i];
-			pastErr_H[i-1] = pastErr_H[i];
-		}
-		// Finish up for loop
-		errSum += error;
-		pastErr_H[0] = error;
-
-		pVal = pidConsts.Kp_H * error;
-		iVal = pidConsts.Ki_H * errSum / SAMPLEFREQ;
-		dVal = pidConsts.Kd_H * (error - pastErr_H[1]) * SAMPLEFREQ;
 	}
-
-	//pVal + iVal + dVal for lower link
-	if (link == 'L') {
-
-		// sum past errors
-		// put current error into array and shift down
-		for (i = NUMERRS; i > 0; i--) {
-			errSum += pastErr_L[i];
-			pastErr_L[i-1] = pastErr_L[i];
-		}
-		// finish up for loop
-		errSum += error;
-		pastErr_L[0] = error;
-
-		// calc values according to equation
-		pVal = pidConsts.Kp_L * error;
-		iVal = pidConsts.Ki_L * errSum / SAMPLEFREQ;
-		dVal = pidConsts.Kd_L * (error - pastErr_L[1]) * SAMPLEFREQ;
+	else
+	{
+		pidValue = pidPrev_H + pidConsts.Kp_H * ((1 + pidTi_inv_H + pidTd_H) * error  + (-1 - 2 * pidTd_H) * pidLastError_H + pidTd_H * pidPrevLastError_H);
+		pidPrev_H = pidValue;
+		pidPrevLastError_H = pidLastError_H;
+		pidLastError_H = error;
 	}
-
-
-	if (pVal >= 4096) {
-		pVal = 4095;
-	} else if (pVal <= -4096) {
-		pVal = -4095;
-	}
-	if (iVal >= 4096) {
-		iVal = 4095;
-	} else if (iVal <= -4096) {
-		iVal = -4095;
-	}
-	if (dVal >= 4096) {
-		dVal = 4095;
-	} else if (dVal <= -4096) {
-		dVal = -4095;
-	}
-
-	//printf("pVal: %ld iVal: %ld dVal: %ld ", pVal, iVal, dVal);
-	return pVal + iVal + dVal;
+	if(pidValue>4000) pidValue=4000;
+	if(pidValue<-4000) pidValue=-4000;
+	return pidValue;
 }
-
-//#endif /* PERIPH_H_ */
